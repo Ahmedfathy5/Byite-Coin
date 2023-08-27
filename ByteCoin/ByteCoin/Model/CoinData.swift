a0816e6e0b35b3374934a6166a97be24522a026a
Ahmed Fathy <af446969@gmail.com>
2023-08-27 18:31:32 +0300

    firt commit

---
+++ ByteCoin/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,12 @@
+//
+//  currencyData.swift
+//  ByteCoin
+//
+//  Created by Ahmed Fathi on 21/08/2023.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+struct CoinData: Decodable {
+    var rate: Double
+}
